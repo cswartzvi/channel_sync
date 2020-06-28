3ada687be1189680f73b251eae870f09809bc5d9
Charles Swartz <charles.w.swartz.vi@gmail.com>
2020-06-28 07:51:12 -0400

    Added merge and dump functionality

--- isoconda/repo.py
+++ isoconda/repo.py
@@ -2,6 +2,9 @@
 from __future__ import annotations
 import collections
 import copy
+import itertools
+import reprlib
+import weakref
 from typing import (
     Any,
     Dict,
@@ -9,6 +12,7 @@ from typing import (
     Iterator,
     List,
     Mapping,
+    MutableSet
 )
 
 from isoconda.errors import InvalidRepo
@@ -36,15 +40,16 @@ class PackageRecord:
         self._pkey = (self.subdir, self.name, self.version,
                       self.build_number, self.build)
         self._hash = hash(self._pkey)
+        self._conda = filename.endswith('conda')
 
     @property
     def build(self) -> str:
-        """Package build identifier."""
+        """The package build identifier."""
         return self._data['build']
 
     @property
     def build_number(self) -> int:
-        """Incremental number for new builds of the same build identifier."""
+        """An incremental number for new builds of the same build identifier."""
         return self._data['build_number']
 
     @property
@@ -59,8 +64,13 @@ class PackageRecord:
 
     @property
     def name(self) -> str:
-        """The package name."""
+        """The name of the package type (generic; different than filename)."""
         return self._data['name']
+
+    @property
+    def sha256(self) -> str:
+        """The sha256 hash code of the package."""
+        return self._data['sha256']
 
     @property
     def subdir(self) -> str:
@@ -68,9 +78,22 @@ class PackageRecord:
         return self._data['subdir']
 
     @property
+    def timestamp(self) -> int:
+        """The timestap of the package."""
+        return self._data['timestamp']
+
+    @property
     def version(self) -> str:
         """The package version."""
         return self._data['version']
+
+    def is_conda(self) -> bool:
+        """Returns True if the package is in the conda format."""
+        return self._conda
+
+    def dump(self) -> Dict[str, Any]:
+        """Converts data into a dictionary representation."""
+        return copy.deepcopy(self._data)
 
     def __eq__(self, other):
         return self._pkey == other._pkey
@@ -116,6 +139,7 @@ class RepoData(Mapping[str, Iterable[PackageRecord]]):
 
         subdir = repodata['info']['subdir']
         package_groups: Dict[str, List[PackageRecord]] = collections.defaultdict(list)
+        packages: MutableSet[PackageRecord] = weakref.WeakSet()
 
         keys = ['packages', 'packages.conda']
         if prefer_conda:
@@ -124,7 +148,9 @@ class RepoData(Mapping[str, Iterable[PackageRecord]]):
         for key in keys:
             for filename, data in repodata[key].items():
                 package = PackageRecord(filename, data)
-                package_groups[package.name].append(package)
+                if package not in packages:
+                    package_groups[package.name].append(package)
+                    packages.add(package)
 
         return cls(subdir, package_groups)
 
@@ -133,18 +159,56 @@ class RepoData(Mapping[str, Iterable[PackageRecord]]):
         """Repository sub-directory (platfrom architecture)."""
         return self._subdir
 
+    def dump(self) -> Dict[str, Any]:
+        """Converts data into a dictionary representation."""
+        data: Dict[str, Any] = {}
+        data['info'] = {'subdir': self.subdir}
+
+        packages: Dict[str, Any] = {}
+        conda_packages: Dict[str, Any] = {}
+        for package in itertools.chain.from_iterable(self.values()):
+            if package.is_conda():
+                conda_packages[package.filename] = package.dump()
+            else:
+               packages[package.filename] = package.dump()
+
+        data['packages'] = packages
+        data['packages.conda'] = conda_packages
+        data['removed'] = []
+        data['repodata_version'] = self.VERSION
+
+        return data
+
+    def merge(self, other: RepoData) -> RepoData:
+        if self.subdir != other.subdir:
+            subdirs = ','.join([self.subdir, other.subdir])
+            raise ValueError(f"Merged subdirs must match: {subdirs})")
+
+        package_groups: Dict[str, List[PackageRecord]] = {}
+        keys = set(self.keys()).union(other.keys())
+        for key in keys:
+            packages = set(self.get(key, [])) | set(other.get(key, []))
+            package_groups[key] = sorted(packages, key=lambda pkg: pkg.filename)
+
+        return RepoData(self.subdir, package_groups)
+
+
     def __contains__(self, key) -> bool:
-        """Checks for membership of a generic package type."""
         return key in self._package_groups.keys()
 
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return ((self.subdir==other.subdir) and
+                    (self._package_groups==other._package_groups))
+
     def __getitem__(self, key) -> Iterable[PackageRecord]:
-        """Yields all records of given package type (name)."""
         return iter(self._package_groups[key])
 
     def __iter__(self) -> Iterator[str]:
-        """Yields all package types (names)."""
         yield from self._package_groups.keys()
 
     def __len__(self) -> int:
-        """Returns the number of package types (names)."""
         return len(self._package_groups.keys())
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self.subdir!r}, ...)'
