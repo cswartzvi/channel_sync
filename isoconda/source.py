6495262929ddbf9a8dc221e5062170f8a1352d56
Charles Swartz <charles.w.swartz.vi@gmail.com>
2020-07-01 06:10:01 -0400

    Added stub for local source

--- isoconda/source.py
+++ isoconda/source.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
+import pathlib
 from typing import Iterable, List, Optional
 
+import json
 import requests
 
 from isoconda.models.channel import ChannelData
 from isoconda.models.repo import RepoData
 
 
-class Source:
+class Cloud:
 
     def __init__(self, location: str, channel: ChannelData, repos: Iterable[RepoData]):
         self._location = location
@@ -15,7 +17,7 @@ class Source:
         self._repos = list(repos)
 
     @classmethod
-    def from_channel(cls, location: str, subdirs: Optional[Iterable[str]] = None) -> Source:
+    def from_source(cls, location: str, subdirs: Optional[Iterable[str]] = None) -> Cloud:
 
         with requests.Session() as session:
             data = session.get(urljoin(location, 'channeldata.json')).json()
@@ -29,7 +31,34 @@ class Source:
                 repos.append(RepoData.from_data(data))
 
             channel = channel.rescale(repos)
-        return Source(location, channel, repos)
+        return Cloud(location, channel, repos)
+
+    def difference(self, local: Local) -> Cloud:
+        pass
+
+
+class Local:
+
+    def __init__(self, location: str, channel: ChannelData, repos: Iterable[RepoData]):
+        self._location = location
+        self._channel = channel
+        self._repos = list(repos)
+
+    def from_source(cls, location: str, subdirs: Optional[Iterable[str]] = None) -> Cloud:
+
+        path = pathlib.Path(location)
+        data = json.load((path / 'channeldata.json').open('rt'))
+        channel = ChannelData.from_data(data)
+        if subdirs is None:
+            subdirs = channel.subdirs
+
+        repos: List[RepoData] = list()
+        for subdir in subdirs:
+            data = json.load((path / subdir / 'repodata.json').open('rt'))
+            repos.append(RepoData.from_data(data))
+
+        channel = channel.rescale(repos)
+        return Cloud(location, channel, repos)
 
 
 def urljoin(*parts):
