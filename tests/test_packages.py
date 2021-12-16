a36220fb2caa197ad88a36971db3e65685845730
Charles Swartz <charles.w.swartz.vi@gmail.com>
2021-12-15 22:02:30 -0500

    Initial work updating tests

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -1,12 +1,21 @@
+from pathlib import Path
+
+import pytest
+from conda.exports import MatchSpec
+
 from isoconda.packages import solve
 from isoconda.repo import fetch_local_records
-from isoconda._vendor.conda.models.match_spec import MatchSpec
 
 
 def test_package_filtering():
-    expected = set(fetch_local_records("./tests/data/expected"))
-    packages = fetch_local_records("./tests/data/pool")
+    expected = set(fetch_local_records(Path("./tests/data/expected")))
+    packages = fetch_local_records(Path("./tests/data/pool"))
     actual = solve(
         [MatchSpec("python >=3.8,<=3.9.0a0"), MatchSpec("numpy >=1.20")], packages
     )
     assert expected == actual
+
+
+@pytest.mark.slow
+def test_solver_satisfies_packages_specs():
+    pass
